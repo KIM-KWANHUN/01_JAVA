84e1183c17e4a5f56e62c016a2283aedc8d02849
KIM-KWANHUN <rhksgns4@gmail.com>
2024-08-26 15:19:52 +0900

    [24.08.26수업자료]

---
+++ chap00-test-lecture-source/src/main/java/com/ohgiraffers/test/Application.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.test;
+
+public class Application {
+
+
+    // main 누르면
+    /* 필기
+     // main 영역은 프로그램을 동작 시킬수있는 영역이다.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("안녕하세요");
+        System.out.println("김관훈입니다.");
+        //System.out.println(안녕하세요);
+
+
+    }
+
+
+}
+
