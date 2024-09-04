2ff4ad6890cef015dab2aafb23504482e530527d
KimKwanHun <rhksgns4@gmail.com>
2024-09-04 17:22:25 +0900

    <24.09.04 배열>

--- cahp05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Test3.java
+++ cahp05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/Test3.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class Test3 {
 
     public static void main(String[] args) {
-
-        String[] str1 = {"딸기","바나나","복숭아","키위","사과"};
+//        int[] iarr3 = new int[] {11, 22, 33, 44, 55};
+        String[] str1 = new String[] {"딸기","바나나","복숭아","키위","사과"};
         Scanner sc = new Scanner(System.in);
         System.out.print("0부터 4까지의 정수를 입력하세요 : ");
         int sum1 = sc.nextInt();
 
-        for(int i = 0; i < str1.length; i++);
-        sum1 += str1.length;
-        System.out.println(str1.length);
+
+            if(sum1 <= 4 && sum1 >= 0){
+                System.out.println("준비된 과일이 나왔습니다.");
+                System.out.println(str1[sum1]);
+            }else{
+                System.out.println("준비된 과일이 없습니다.");
+            }
+
+
 
 
         }
