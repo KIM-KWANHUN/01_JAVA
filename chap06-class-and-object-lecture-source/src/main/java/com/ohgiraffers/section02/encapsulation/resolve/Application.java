b7c77a2a27e22f56c7a20b20e4cafc1bf61a714d
KimKwanHun <rhksgns4@gmail.com>
2024-09-06 12:49:00 +0900

    <24.09.06 클래스와 객체>

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/resolve/Application.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.section02.encapsulation.resolve;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* title. 접근제한자를 이해하고 지금까지 발생한 문제를 해결할 수 있다. */
+
+    }
+
+}
