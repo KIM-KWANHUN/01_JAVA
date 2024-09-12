d6070364055ad3806b65356a9bc1928a155d0d10
KimKwanHun <rhksgns4@gmail.com>
2024-09-12 11:53:19 +0900

    <24.09.12 API>

---
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use/Application.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section04.use;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section04/use/Car.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section04.use;
+
+public abstract class Car {
+
+    public abstract void go(); //추상 메소드 작성 abstract
+
+    public abstract void stop();
+}
