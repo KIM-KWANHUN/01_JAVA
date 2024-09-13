5219c5889a53eca52290daa7e44954955ff3bbe2
KimKwanHun <rhksgns4@gmail.com>
2024-09-13 13:12:15 +0900

    <24.09.13 제너릭>

--- chap11-generic-lecture-source/src/main/java/com/ohgiraffers/section02/extend/run/Application02.java
+++ chap11-generic-lecture-source/src/main/java/com/ohgiraffers/section02/extend/run/Application02.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.extend.run;
 
+import com.ohgiraffers.section02.extend.*;
+
 public class Application02 {
 
     public static void main(String[] args) {
@@ -12,8 +14,28 @@ public class Application02 {
         *   그 객체의 타입 변수를 제한할 수 있다.
         *   <?> : 제한 없음 (다 가능)
         *   <? extend Type> : 와일드카드 상한 제한(Type 과 Type 의 후손을 이용한 인스턴스만 인자로 사용 가능)
-        *   <? super Type> : 
+        *   <? super Type> : 와일드카드 하한 제한(Type 과 Type 의 부모를 이용한 인스턴스만 인자로 사용 가능)
+        *
         *    */
+
+        WildCardFarm wildCardFarm = new WildCardFarm();
+
+        wildCardFarm.anyType(new RabbitFarm<>(new Rabbit()));
+        wildCardFarm.anyType(new RabbitFarm<>(new Bunny()));
+        wildCardFarm.anyType(new RabbitFarm<>(new DrunkenBunny()));
+
+        /* comment.
+        *   Bunny 이거나 Bunny 의 후손 토끼 농장만 매개변수로
+        *   사용이 가능하고 상위 타입인 Rabbit 은 매개변수로
+        *   사용이 불가능하다. */
+
+        // wildCardFarm.extendsType(new RabbitFarm<>(new Rabbit()));
+        wildCardFarm.extendsType(new RabbitFarm<>(new Bunny()));
+        wildCardFarm.extendsType(new RabbitFarm<>(new DrunkenBunny()));
+
+        wildCardFarm.superType(new RabbitFarm<>(new Rabbit()));
+        wildCardFarm.superType(new RabbitFarm<>(new Bunny()));
+        wildCardFarm.superType(new RabbitFarm<>(new DrunkenBunny()));
 
     }
 }
