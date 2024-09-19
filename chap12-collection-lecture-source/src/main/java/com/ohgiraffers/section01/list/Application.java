8d3f9690bfd2286dba8ea654e41385c66ec44275
KimKwanHun <rhksgns4@gmail.com>
2024-09-19 11:30:20 +0900

    <24.09.19 컬렉션>

--- chap12-collection-lecture-source/src/main/java/com/ohgiraffers/section01/list/Application.java
+++ chap12-collection-lecture-source/src/main/java/com/ohgiraffers/section01/list/Application.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.section01.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class Application {
@@ -63,6 +65,65 @@ public class Application {
         *   List 인터페이스 하위의 다양한 구현체들로 타입 변경이 가능하기 때문에
         *   레퍼런스 타입은 List 로 하는 것이 더 유연한 코드를 작성하는 방법이다. */
         List list = new ArrayList();
+
+        // List 에 값을 추가하는 방법
+        alist.add("apple");
+        alist.add(123); // auto-boxing 처리가 됨
+        alist.add(45.67); // int, double 이러한 기본(원시) 자료형 값을 객체 - Integer, Double 로 바꿔줌
+        alist.add(new Date());
+
+        // toString 메소드가 OverRiding 되어 있기 때문에 주소값이 출력이 안되고, 값이 나오고 있다.
+        // 출력해보면 우리가 작성한 순서대로 값이 들어있는 것을 확인 할 수 있다.
+        System.out.println("alist = " + alist);
+
+        System.out.println("alist 의 크기 : " + alist.size());
+
+        // 인덱스로 관리를 한다 ? -> for 문 즉 반복문을 사용해 규칙적으로 접근 가능
+        for(int i = 0; i < alist.size(); i++) {
+            // 인덱스에 해당하는 값을 가져올 땐 get() 메소드를 사용한다.
+            System.out.println(i+ "번 째 인덱스의 값 : " + alist.get(i));
+        }
+        
+        // 데이터의 중복저장 허용 확인
+        alist.add("apple");
+        System.out.println("alist = " + alist); // 중복된 값을 동일하게 넣을 수 가 있다.
+
+        // 원하는 인덱스 위치에 값을 추가해보기
+        // 새로운 값이 들어갈 인덱스를 지정하면
+        // 한 칸씩 밀려서 값이 들어가게된다.
+        alist.add(1,"banana");
+        System.out.println("alist = " + alist);
+
+        // 지정된 위치에 값을 삭제
+        alist.remove(1);
+
+        System.out.println("alist = " + alist);
+
+        // 지정된 위치에 값을 수정
+        alist.set(1,Boolean.valueOf(true));
+        System.out.println("alist = " + alist);
+
+        /* comment.
+            모든 컬렉션 프레임워크 클래스는 제네릭클래스로 지정됨
+        *   따라서 우리가 <> 내부에 타입을 지정하면
+            지정한 타입외에 지정할 수 없다.*/
+        List<String> stringList = new ArrayList<>();
+        stringList.add("apple");
+        // 제네릭 문법으로 타입을 지정하게 되면. 지정한타입외에 값은 저장할 수 없다.
+       // stringList(123);
+        stringList.add("orange");
+        stringList.add("banana");
+        stringList.add("mango");
+        stringList.add("grape");
+
+        System.out.println("stringList = " + stringList);
+        
+        /* comment.
+        *   정렬 기능도 사용할 수 있다.
+        *   저장 순서를 유지하고 있는 stringList를 오름차순 정렬 */
+
+        Collections.sort(stringList);
+        System.out.println("stringList = " + stringList);
 
 
     }
