c6b8e49a3838180dc1b03e6d06f0633b98f8ea6e
KimKwanHun <rhksgns4@gmail.com>
2024-09-23 11:33:09 +0900

    24.09.23 <입출력>

--- chap14-io-lecture-source/src/main/java/com/ohgiraffers/section02/stream/Application02.java
+++ chap14-io-lecture-source/src/main/java/com/ohgiraffers/section02/stream/Application02.java
@@ -1,5 +1,9 @@
 package com.ohgiraffers.section02.stream;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Application02 {
 
     public static void main(String[] args) {
@@ -11,6 +15,33 @@ public class Application02 {
         *   하지만 큰 차이점은
         *   byte 단위가 아닌 character 단위로 읽어들이는 부분이
         *   가장 큰 차이점이다. */
+
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader("src/main/java/com/ohgiraffers/section02/stream/testReader.txt");
+
+            int value;
+            while((value = fr.read()) != -1) {
+                System.out.println((char)value);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            if(fr != null) {
+                try {
+                    fr.close();
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 
     }
 }
